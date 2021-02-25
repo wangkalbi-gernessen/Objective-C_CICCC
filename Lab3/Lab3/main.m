a585a89e315363121c8f367400cd00207f2f6fcf
wangkalbi-gernessen <guizhou08chishui@gmail.com>
2021-02-25 12:59:04 -0800

    Add some NSLogs on main.m

--- Lab3/Lab3/main.m
+++ Lab3/Lab3/main.m
@@ -10,39 +10,44 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        NSLog(@"MATHS! There are 10 questions.");
-        int score = 0;
-        int count = 10;
-        while(count > 0) {
-            NSInteger first = arc4random_uniform(100) + 10;
-            NSInteger second = arc4random_uniform(100) + 10;
-            AdditionQuestion *addQuestion = [[AdditionQuestion alloc]  initWithLeft:first right:second];
-            
-            NSLog(@"%@", addQuestion.question);
-            char buf[10];
-            char *result = fgets(buf,10,stdin);
-            // convert char *result to NSString
-            NSString *str = [NSString stringWithUTF8String: buf];
-            // remove all whitespaces of *str
-            NSCharacterSet *whitespaces = [NSCharacterSet whitespaceAndNewlineCharacterSet];
-            NSString *trimmedInput = [str stringByTrimmingCharactersInSet: whitespaces];
-            
-            // stop game
-            if ([trimmedInput isEqualToString:@"quit"]) {
-                break;
-            // continue game
-            } else {
-                if (trimmedInput.intValue == addQuestion.answer) {
-                    NSLog(@"Right!");
-                    score++;
-                } else if ((trimmedInput.intValue == nil || trimmedInput.intValue != addQuestion.answer)) {
-                    NSLog(@"Wrong!");
+        while(YES) {
+            NSLog(@"MATHS! There are 10 questions.");
+            int right = 0;
+            int wrong = 0;
+            int count = 10;
+            while(count > 0) {
+                NSInteger first = arc4random_uniform(100) + 10;
+                NSInteger second = arc4random_uniform(100) + 10;
+                AdditionQuestion *addQuestion = [[AdditionQuestion alloc]  initWithLeft:first right:second];
+                
+                NSLog(@"%@ Please enter 'quit' to quit", addQuestion.question);
+                char buf[10];
+                char *result = fgets(buf,10,stdin);
+                // convert char *result to NSString
+                NSString *str = [NSString stringWithUTF8String: buf];
+                // remove all whitespaces of *str
+                NSCharacterSet *whitespaces = [NSCharacterSet whitespaceAndNewlineCharacterSet];
+                NSString *trimmedInput = [str stringByTrimmingCharactersInSet: whitespaces];
+                
+                // stop game
+                if ([trimmedInput isEqualToString:@"quit"]) {
+                    NSLog(@"Finish Game");
+                    break;
+                // continue game
+                } else {
+                    if (trimmedInput.intValue == addQuestion.answer) {
+                        NSLog(@"Right!");
+                        right++;
+                    } else if ((trimmedInput.intValue == nil || trimmedInput.intValue != addQuestion.answer)) {
+                        NSLog(@"Wrong!");
+                        wrong++;
+                    }
                 }
-            }
-            count--;
-            
-            if (count == 0) {
-                NSLog(@"Score is %d right / 10 questions", score);
+                count--;
+                
+                if (count == 0) {
+                    NSLog(@"Score is %d right & %d wrong / 10 questions", right, wrong);
+                }
             }
         }
    }
